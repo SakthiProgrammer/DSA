aba61efaa73d623bba9d8629d18483ec0154bab6
Sakthiprogrammer <sakthiveld479@gmail.com>
2025-05-25 13:06:48 +0530

    Feat: New Stack and Queue Problems Added..

--- Problem Solving/YoutubeProblems/Stack/StackCall.java
+++ Problem Solving/YoutubeProblems/Stack/StackCall.java
@@ -8,7 +8,7 @@ public class StackCall implements MethodCall {
     public void call() {
 
         Stacks stack = new Stacks();
-        /* ============================================== P: 16 */
+        /* ============================================== P: 30 */
         // 1. Valid Parantheses
         /* ============================================== */
 
@@ -27,6 +27,22 @@ public class StackCall implements MethodCall {
         inpStr = "([])";
         resBool = stack.isValid(inpStr);
         System.out.println("Valid Parantheses: " + resBool);// true
+
+        /* ============================================== P: 31 */
+        // 2. Evaluate Reverse Polish Notation
+        /* ============================================== */
+
+        String[] tokens1 = { "2", "1", "+", "3", "*" };
+        int result1 = stack.evalRPN(tokens1);
+        System.out.println("Eval RPN [2 1 + 3 *]: " + result1); // 9
+
+        String[] tokens2 = { "4", "13", "5", "/", "+" };
+        int result2 = stack.evalRPN(tokens2);
+        System.out.println("Eval RPN [4 13 5 / +]: " + result2); // 6
+
+        String[] tokens3 = { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
+        int result3 = stack.evalRPN(tokens3);
+        System.out.println("Eval RPN [10 6 9 3 + -11 * / * 17 + 5 +]: " + result3); // 22
 
     }
 
